a62d2a60e57f91e64e751de2b41d569186b272e1
Suvankit Sahoo <85415546+suvankit99@users.noreply.github.com>
2024-11-26 21:18:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ const io = require('socket.io')(server , {
 
     pingTimeout : 60000 , 
     cors : {
-        origin : "http://localhost:3000" // now our server acccepts https requests from client which is located at localhost:3000
+        origin : "*" // now our server acccepts https requests from client which is located at localhost:3000
     }
 })
 
